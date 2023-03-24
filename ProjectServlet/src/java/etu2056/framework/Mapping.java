1b5dedf29687875cb8fd5463328384b2a907775a
rjratefy <118510731+rjratefy@users.noreply.github.com>
2023-03-24 06:34:44 +0100

    Sprint3

--- ProjectServlet/src/java/etu2056/framework/Mapping.java
+++ ProjectServlet/src/java/etu2056/framework/Mapping.java
@@ -1,7 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+*/ 
+
 package etu2056.framework;
 
 /**
@@ -9,8 +10,19 @@ package etu2056.framework;
  * @author jaona
  */
 public class Mapping { 
-     String className;
+    String className;
     String Method;
-    
+
+    public Mapping(String simpleName, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getMethod() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getClassName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
--- ProjectServlet/src/java/etu2056/framework/servlet/FrontServlet.java
+++ ProjectServlet/src/java/etu2056/framework/servlet/FrontServlet.java
@@ -13,6 +13,8 @@ package etu2056.framework.servlet;
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+import etu2056.AllAnnotations.Method;
+import etu2056.framework.Annotation;
 import etu2056.framework.Mapping;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -21,13 +23,19 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 /**
  *
  * @author jaona
  */
-public class FrontServlet extends HttpServlet {
-    HashMap<String,Mapping> MappingUrls;
+public class FrontServlet extends HttpServlet { 
+     HashMap<String,Mapping> MappingUrls;
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -53,7 +61,62 @@ public class FrontServlet extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
         }
+    }  
+    
+     @Override
+    public void init(){
+        try{
+            //System.out.println("etu2056.framework.servlet.FrontServlet.init()");
+            Annotation a = new Annotation();
+            Vector<Class> vec = a.getClassFrom("etu2056.models");
+            for(int i = 0; i < vec.size(); i++) { 
+                if(vec.get(i) != null) {
+                    insertHashMap(vec.get(i));
+                }
+            }
+            
+            afficherHashMap();
+        }
+        catch(Exception e){
+            try {
+                throw e;
+            } catch (Exception ex) {
+                Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+       }
+//       
+        public void insertHashMap(Class<?> className) {
+         for (java.lang.reflect.Method declaredMethod : className.getDeclaredMethods()) {
+             //System.out.println("tonga1 : "+ className.getDeclaredMethods()[i].getName());
+             if (declaredMethod.getAnnotation(Method.class) != null) {
+                 String url = declaredMethod.getAnnotation(Method.class).name_method();
+                 //System.out.println(url);
+                 this.MappingUrls = new HashMap();
+                 this.MappingUrls.put(url, new Mapping(className.getSimpleName(), declaredMethod.getName()));
+             }
+         }
     }
+//       
+       public void afficherHashMap(){
+           for ( HashMap.Entry<String, Mapping> en : this.MappingUrls.entrySet()) {
+            System.out.println(" Le nom de la classe : "+en.getValue().getClassName());
+            System.out.println(" La methode : "+en.getValue().getMethod()); 
+           }
+       }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+    
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -67,7 +130,8 @@ public class FrontServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+            processRequest(request, response); 
+        
     }
 
     /**
@@ -92,6 +156,12 @@ public class FrontServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }// </editor-fold>  
+    
+    
+   
+    
+    
+    
 
 }
